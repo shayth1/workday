fa001a2f576ecc3a009d69350504f324c8aa1b5c
shayth1 <shaythwork@gmail.com>
2022-05-30 18:47:21 +0300

    add data base

--- assets/setup/workday.sql
+++ assets/setup/workday.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 25, 2022 at 12:29 PM
--- Server version: 10.4.21-MariaDB
--- PHP Version: 7.4.25
+-- Generation Time: May 30, 2022 at 05:45 PM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 7.4.27
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -42,8 +42,7 @@ CREATE TABLE `auth_tokens` (
 --
 
 INSERT INTO `auth_tokens` (`id`, `user_email`, `auth_type`, `selector`, `token`, `created_at`, `expires_at`) VALUES
-(64, 'shayth@shayth.com', 'account_verify', 'b8864e3fc97d9d9a', '$2y$10$J.C.WVA1zFTGazNyPip1Q.8uSe50ylX/s5pJ.R7IrEa1yyW5gYV76', '2022-03-23 17:27:15', '2022-03-23 18:27:15'),
-(65, 'shayth@shayth.com', 'remember_me', 'e3cd7f07e594480e', '$2y$10$XUZTQV.kTE9FgqGeFrTdAuCQtUisv4AzgMd3YgrOI4LgeLrEoCOre', '2022-03-23 17:27:22', '2022-04-02 16:27:22');
+(66, 'fatima@fatima.com', 'account_verify', 'a2b52a706e00e76d', '$2y$10$6S3Tp/h0ac1Cj1yJysPEI.KyA/saHxPZukfq778qMKRCJBlmPQyCu', '2022-05-28 18:51:34', '2022-05-28 19:51:34');
 
 -- --------------------------------------------------------
 
@@ -64,7 +63,10 @@ CREATE TABLE `comment` (
 
 INSERT INTO `comment` (`comment_id`, `comment_task`, `comment_user`, `comment_text`) VALUES
 (1, 1, 31, 'test Comment'),
-(2, 1, 32, 'a7a');
+(3, 11, 31, 'its done '),
+(4, 12, 31, ''),
+(5, 12, 31, 'its done'),
+(6, 12, 32, 'no we have ticket in line 27 in connect');
 
 -- --------------------------------------------------------
 
@@ -87,7 +89,15 @@ CREATE TABLE `project` (
 --
 
 INSERT INTO `project` (`project_id`, `project_name`, `project_sdate`, `project_edate`, `project_owner`, `project_status`, `project_logo`) VALUES
-(1, 'ssss', '2022-03-03', '2022-03-23', 31, 'Active', '623576536ab322.03986307.jpeg');
+(2, 'liiinaa', '2022-05-27', '2022-05-30', 31, 'Active', 'project.png'),
+(3, 'test', '2022-05-28', '2022-05-30', 33, 'Active', 'project.png'),
+(4, 'testing', '2022-05-28', '2022-05-30', 31, 'Active', 'project.png'),
+(5, 'yaqout', '2022-05-28', '2022-05-31', 31, 'Active', 'project.png'),
+(6, 'fatima', '2022-05-29', '2022-05-31', 31, 'Active', 'project.png'),
+(7, 'YAQOOT TEST', '2022-05-30', '2022-06-11', 31, 'Active', 'project.png'),
+(8, 'fatima Project 22', '2022-05-10', '2022-05-31', 31, 'Active', 'project.png'),
+(9, ' Project', '0000-00-00', '0000-00-00', 32, 'Active', 'project.png'),
+(10, '2222', '0000-00-00', '0000-00-00', 31, 'Active', 'project.png');
 
 -- --------------------------------------------------------
 
@@ -115,10 +125,15 @@ CREATE TABLE `task` (
 
 INSERT INTO `task` (`task_id`, `task_project`, `task_name`, `task_cdate`, `task_estimated`, `task_actual`, `assign_to`, `task_discription`, `priority`, `task_status`, `task_due`) VALUES
 (1, 1, 'Task One', '2019-03-22', 24, 7, 31, 'discription', 'Medium', 'in-Progress', NULL),
-(2, 1, '', '0000-00-00', 0, NULL, NULL, 'discription', 'Urgent', 'New', NULL),
 (3, 1, 'Task 2', '0000-00-00', 1.5, NULL, NULL, 'discription', 'Low', 'New', NULL),
 (4, 1, 'Task 4', '2022-03-19', 3.5, NULL, NULL, 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut aliquid ea eligendi fuga aspernatur dolorum optio molestias, incidunt iure alias voluptatum quidem odio provident, a quam rem neque recusandae deleniti.', 'Urgent', 'New', NULL),
-(5, 1, 'Fainal Test', '2022-03-25', 12, 2, 32, 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae similique dignissimos vero nam laudantium laborum fugit enim tempora ducimus? Repellendus, cum ratione itaque omnis dignissimos odio officia reprehenderit dolores sint.', 'Medium', 'New', '2022-03-31');
+(6, 1, 'fatima', '2022-05-28', 24, 20, 31, 'test', 'Medium', 'New', '2022-05-28'),
+(7, 3, 'fatima', '2022-05-28', 24, 15, 32, 'test', 'Medium', 'New', '2022-05-28'),
+(8, 5, 'test 2', '2022-05-28', 48, NULL, 32, '', 'High', 'New', '2022-05-28'),
+(9, 6, '', '2022-05-28', 72, NULL, NULL, '', 'Urgent', 'New', '2022-05-25'),
+(10, 5, 'login page', '2022-05-28', 72, 70, 32, '', 'Urgent', 'New', '2022-05-25'),
+(11, 5, 'front end ', '2022-05-28', 0, 0, 32, 'add colors ', 'High', 'New', '2022-06-06'),
+(12, 7, 'software test', '2022-05-30', 29, NULL, 32, 'design homepage', 'Medium', 'Testing', '2022-05-30');
 
 -- --------------------------------------------------------
 
@@ -149,8 +164,9 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `username`, `email`, `password`, `first_name`, `last_name`, `gender`, `headline`, `bio`, `profile_image`, `verified_at`, `created_at`, `updated_at`, `deleted_at`, `last_login_at`) VALUES
-(31, 'admin', 'admin@admin.com', '$2y$10$jhIOk4NVdBile/NwhAU9We/f0aoohx.cG9CizmIALRz0aCKJa5s6a', 'Supahot', 'Soverysupahot', 'm', '0799978266', 'React, .Net, C#, ABAP, SAP,Angular', '623b78fcdfe9a3.50873298.png', '2022-03-09 11:41:52', '2022-03-18 10:25:25', '2022-03-25 11:13:35', NULL, '2022-03-19 05:16:03'),
-(32, 'shayth', 'shayth@shayth.com', '$2y$10$jnRMeOqpCa20aY1Xrkq8luoZx116hFxskbuRKahWWx4nxbD4XUdpW', 'shayth', 'bani baker', 'm', '0799978266', 'React, .Net, C#, ABAP, SAP,Angular', '_defaultUser.png', '2022-03-23 17:27:15', '2022-03-23 17:27:15', '2022-03-25 11:19:41', NULL, '2022-03-25 11:19:41');
+(31, 'lina', 'admin@admin.com', '$2y$10$jhIOk4NVdBile/NwhAU9We/f0aoohx.cG9CizmIALRz0aCKJa5s6a', 'lina', 'hamdan', 'f', '0796104250', 'React, .Net, C#, ABAP, SAP,Angular', '62931f5cb3a0b0.46501713.png', '2022-03-09 11:41:52', '2022-03-18 10:25:25', '2022-05-30 14:41:40', NULL, '2022-05-30 14:41:40'),
+(32, 'yaqout', 'yaqout@yaqout.com', '$2y$10$jhIOk4NVdBile/NwhAU9We/f0aoohx.cG9CizmIALRz0aCKJa5s6a', 'yaqout', 'salahat', 'f', '0781111710', 'React, .Net, C#, ABAP, SAP,Angular', 'a.png', '2022-03-23 17:27:15', '2022-03-23 17:27:15', '2022-05-30 14:42:01', NULL, '2022-05-30 14:42:01'),
+(33, 'fatima', 'fatima@fatima.com', '$2y$10$EqIz44Q7/QReQaiCkRQVku1kpGvpei.JX7UINtAq23wQaKLB/FO7a', 'fatima', 'mustafa', 'f', '0791526367', '', '62927095cfdd82.93591027.png', '2022-05-28 18:51:33', '2022-05-28 18:51:33', '2022-05-28 19:06:10', NULL, '2022-05-28 19:06:10');
 
 --
 -- Indexes for dumped tables
@@ -198,31 +214,31 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `auth_tokens`
 --
 ALTER TABLE `auth_tokens`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=66;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=67;
 
 --
 -- AUTO_INCREMENT for table `comment`
 --
 ALTER TABLE `comment`
-  MODIFY `comment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `comment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `project`
 --
 ALTER TABLE `project`
-  MODIFY `project_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `project_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `task`
 --
 ALTER TABLE `task`
-  MODIFY `task_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `task_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
